724da2a9d630d61a4f9675f1fc94d122b4ea2aa3
Md Fardin Islam (Rijby Sarkar) <mdfardinislamrijby@gmail.com>
2026-06-05 20:07:24 +0600

    calculate GPA grades

---
+++ ObtainedGpa.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ObtainedGpa {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num>=90){
+            System.out.println("A+");
+        }
+        else if(num>=80){
+            System.out.println("A");
+        }
+        else if(num>=70){
+            System.out.println("A-");
+        }
+        else if(num>=60){
+            System.out.println("B");
+        }
+        else if(num>=40){
+            System.out.println("C+");
+        }
+        else{
+            System.out.println("F");
+        }
+    }
+}
